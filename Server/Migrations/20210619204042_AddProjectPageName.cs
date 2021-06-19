601a970b9a5c02ba23aeb2d1d52fd784bbe02ff7
Robert Hardy <helloarchy@gmail.com>
2021-06-19 21:52:05 +0100

    Add project page name

---
+++ Server/Migrations/20210619204042_AddProjectPageName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Portfolio.Server.Migrations
+{
+    public partial class AddProjectPageName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PageName",
+                table: "Projects",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PageName",
+                table: "Projects");
+        }
+    }
+}
